4b42e91ad766910cd361552bcbad8401398f737f
Hendrik Mann <hendrikm@outlook.de>
2025-07-02 00:20:45 +0200

    posts

--- assets/blog/posts.go
+++ assets/blog/posts.go
@@ -12,19 +12,27 @@ type Post struct {
 
 var Posts = []Post{
 	{
+		Date:           "AUG 26 2024",
+		Header:         "Rewriting R Functions in C",
+		PreviewImage:   "/assets/blog/rewriting/thumbnail.png",
+		PreviewContent: "A guide to using the C API with R to improve performance by rewriting vector operations.",
+		HTMLContent:    "assets/blog/rewriting/content.html",
+		Id:             "rewriting",
+	},
+	{
+		Date:           "JUN 15 2024",
+		Header:         "Introduction to partial residual plots",
+		PreviewImage:   "/assets/blog/partial/thumbnail.png",
+		PreviewContent: "Partial residual plots can give you valuble information about your model and relationships in the data like interactions and nonlinear trends. In this post we look at use cases and different ways PRP can be created in ggplot.",
+		HTMLContent:    "assets/blog/partial/content.html",
+		Id:             "partial",
+	},
+	{
 		Date:           "JUN 12 2024",
 		Header:         "Correlation Networks in R",
 		PreviewImage:   "/assets/blog/networks/thumbnail.png",
-		PreviewContent: "Plotting correlations as networks can give you a good first impression of the interconnectivity of your variables. This is a quick tutorial on how to create correlation networks in R. ",
+		PreviewContent: "Plotting correlations as networks can give you a good first impression of the interconnectivity of your variables. This is a quick tutorial on how to create correlation networks in R.",
 		HTMLContent:    "assets/blog/networks/content.html",
 		Id:             "networks",
-	},
-	{
-		Date:           "JAN 6 2024",
-		Header:         "test 2",
-		PreviewImage:   "/assets/blog/pipeline/preview.png",
-		PreviewContent: "test preview",
-		HTMLContent:    "test content",
-		Id:             "test-post2",
 	},
 }
